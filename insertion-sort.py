5381c8b7301d12db36eeb1f19e4f3e39f8e4879c
JOSEPH KIM <josephkim.dev@gmail.com>
2015-07-04 23:11:39 -0700

    modify insertion sort

--- insertion-sort.py
+++ insertion-sort.py
@@ -1,9 +1,13 @@
 import random
-a = [int(random.uniform(1, 50)) for i in xrange(10)]
-print a
-for i in xrange(len(a)):
-    if i > 0:
-        while a[i-1] >= a[i] and i > 0:
-            a[i-1], a[i] = a[i], a[i-1]
-            i = i-1
-print a
+
+def insert_sort(array):
+    for i in xrange(len(array)):
+        if i > 0:
+            while array[i-1] >= array[i] and i > 0:
+                array[i-1], array[i] = array[i], array[i-1]
+                i = i-1
+
+if __name__ == "__main__":
+    array = [int(random.uniform(1, 50)) for i in xrange(10)]
+    insert_sort(array)
+    print array
